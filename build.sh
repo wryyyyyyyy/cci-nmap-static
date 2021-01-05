10748b03dc74cca7b94444f7a0b94e75eb8aafc9
wryyyyyyyy <no@spam.here>
2021-01-05 01:58:18 +0100

    build.sh changed

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 sudo wget https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/x86_64/nmap -O /usr/bin/nmap && sudo chmod +x /usr/bin/nmap
 sudo wget -O /usr/bin/nmap-services https://svn.nmap.org/nmap/nmap-services
-sudo /usr/bin/nmap -v localhost
+sudo echo "# cci-nmap-static  ![CircleCI](https://circleci.com/gh/wryyyyyyyy/cci-nmap-static.svg?style=shield&circle-token=96870f3cbda563d780f520aa67eb71f915b61f48)" >README.md
+sudo echo '```' >>README.md
+sudo /usr/bin/nmap -v localhost >>README.md
+sudo echo '```' >>README.md
